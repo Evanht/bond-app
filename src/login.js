c50fe3ed365a3c41d30597f709a66d0bf855b7c2
ejaycoleman <ejaycoleman@googlemail.com>
2017-11-18 14:21:07 +0000

    started development of input forms for login page. Logs on form submit

--- src/login.js
+++ src/login.js
@@ -2,14 +2,57 @@
 import React, { Component } from 'react';
 
 class login extends React.Component {
+constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: ''
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleSubmit(event) {
+    console.log("username: " + this.state.username);
+    console.log("password: " + this.state.password);
+    event.preventDefault();
+  }
+
   render() {
     return (
-      <div>
-        <h1>Login Page</h1>
-        <h2>This is where you will login</h2>
-      </div>
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Username:
+          <input
+            name="username"
+            type="text"
+            value={this.state.username}
+            onChange={this.handleInputChange} />
+        </label>
+        <br />
+        <label>
+          Number of guests:
+          <input
+            name="password"
+            type="password"
+            value={this.state.password}
+            onChange={this.handleInputChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
     );
-  }
+   }
 }
 
 export default login;
