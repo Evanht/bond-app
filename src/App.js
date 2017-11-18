a135d2e4fe39b81ecb517bcc696e918327ec9add
Evan Thomas <evanhen@hotmail.co.uk>
2017-11-18 13:06:16 +0000

    styled compon

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Button } from 'antd';
+import styled from 'styled-components'
 import './App.css';
+
 
 class App extends Component {
   render() {
---
+++ src/components/topNav.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Button } from 'antd'
+
+const styledNavWrapper = styled(Button)`
+  background-color: red;
+
+`
+
+export default topNav = () => (
+  <styledNavWrapper />
+)
