4380ef8b260b853712c27b50f4f28eb41fbc4eb0
ejaycoleman <ejaycoleman@googlemail.com>
2017-11-18 16:00:16 +0000

    Working login using antd

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,14 +1,20 @@
-/*
+import api from '../api';
 
 import React, { Component } from 'react';
 
 import { Form, Icon, Input, Button, Checkbox } from 'antd';
+const FormItem = Form.Item;
+
+
+
+
+
 
 class login extends React.Component {
 constructor(props) {
     super(props);
     this.state = {
-      email: '',
+      username: '',
       password: ''
     };
 
@@ -27,102 +33,50 @@ constructor(props) {
   }
 
   handleSubmit(event) {
-    console.log("email: " + this.state.email);
+    console.log("username: " + this.state.username);
     console.log("password: " + this.state.password);
+    
+    api.authenticate({
+      strategy: 'local',
+      email: this.state.email,
+      password: this.state.password
+    });
+
     event.preventDefault();
   }
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Email:
-          <input
-            name="email"
-            type="text"
-            value={this.state.email}
-            onChange={this.handleInputChange} />
-        </label>
+      <Form onSubmit={this.handleSubmit}>
+        <FormItem>
+            <Input 
+              prefix={<Icon type="user" 
+              style={{ fontSize: 13 }} />} 
+              placeholder="Email" 
+              name="text"
+              type="text"
+              value={this.state.email}
+              onChange={this.handleInputChange}
+            />
+        </FormItem>
         <br />
-        <label>
-          Number of guests:
-          <input
-            name="password"
-            type="password"
-            value={this.state.password}
-            onChange={this.handleInputChange} />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
+        <FormItem>
+            <Input 
+              prefix={<Icon type="user" 
+              style={{ fontSize: 13 }} />} 
+              placeholder="Password" 
+              name="password"
+              type="password"
+              value={this.state.password}
+              onChange={this.handleInputChange}
+            />
+        </FormItem>
+        <Button type="primary" htmlType="submit" className="login-form-button">
+          Log in
+        </Button>
+      </Form>
     );
    }
 }
 
 export default login;
-
-*/
-
-
-
-
-
-
-
-
-import React from 'react';
-import { Form, Icon, Input, Button, Checkbox } from 'antd';
-const FormItem = Form.Item;
-
-class login extends React.Component {
-  handleSubmit = (e) => {
-    e.preventDefault();
-    this.props.form.validateFields((err, values) => {
-      if (!err) {
-        console.log('Received values of form: ', values);
-      }
-    });
-  }
-  render() {
-    const { getFieldDecorator } = this.props.form;
-    return (
-      <Form onSubmit={this.handleSubmit} className="login-form">
-        <FormItem>
-          {getFieldDecorator('userName', {
-            rules: [{ required: true, message: 'Please input your username!' }],
-          })(
-            <Input prefix={<Icon type="user" style={{ fontSize: 13 }} />} placeholder="Username" />
-          )}
-        </FormItem>
-        <FormItem>
-          {getFieldDecorator('password', {
-            rules: [{ required: true, message: 'Please input your Password!' }],
-          })(
-            <Input prefix={<Icon type="lock" style={{ fontSize: 13 }} />} type="password" placeholder="Password" />
-          )}
-        </FormItem>
-        <FormItem>
-          {getFieldDecorator('remember', {
-            valuePropName: 'checked',
-            initialValue: true,
-          })(
-            <Checkbox>Remember me</Checkbox>
-          )}
-          <a className="login-form-forgot" href="">Forgot password</a>
-          <Button type="primary" htmlType="submit" className="login-form-button">
-            Log in
-          </Button>
-          Or <a href="">register now!</a>
-        </FormItem>
-      </Form>
-    );
-  }
-}
-
-const WrappedNormalLoginForm = Form.create()(login);
-
-export default login;
-
-
-
-
-
