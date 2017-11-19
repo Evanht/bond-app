d47c494cfcb06928e1d4528c859d4a63dff00b28
ejaycoleman <ejaycoleman@googlemail.com>
2017-11-19 12:49:27 +0000

    buy and sell adopted design from dashboard

--- src/components/buy/buyBondCard.js
+++ src/components/buy/buyBondCard.js
@@ -1,25 +1,22 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
-import { Icon, Modal, Button, notification  } from 'antd'
+import { Icon, Modal, Button, notification, Progress  } from 'antd'
 import { Flex, Box } from 'grid-styled'
 
 
 const BondCard = styled.div `
   box-shadow: 0 0 0 0px rgba(0,0,0,.1), 0 2px 3px rgba(0,0,0,.2);
-  border: thin double #EDEDED;
-  padding: 5px;
+  padding: 5px 5px 5px 0px;
+  background: white;
+  border-left: 4px solid ${({ color }) => color};
+  border-radius: 4px;
   width: 100%;
-  height: 80px;
+  ${'' /* height: 80px; */}
   margin-bottom: 10px;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
 `
 
-const BuyButton = styled.button `
-  width: 20%;
-  height: 100%;
-  background-color: #00A862;
+const BondInfo = styled.div `
+  display: flex;
 `
 
 const Name = styled(Box) `
@@ -41,12 +38,40 @@ const Number = styled(Box) `
   font-size: 15px; 
 `
 
-const InfoButton = styled.button `
-  width: 5%;
-  height: 100%;
-  background-color: #BBB;
-`
 
+
+const percentages = [38, 22, 18, 6, 16, 12, 41, 12, 22, 12]
+const names = [
+  "Zambia Government XL", 
+  "Uganda Infrastructure XL",
+  "Zambia Government XL", 
+  "Uganda Infrastructure XL",
+  "Zambia Government XL", 
+  "Uganda Infrastructure XL",
+  "Zambia Government XL", 
+  "Uganda Infrastructure XL",
+  "Zambia Government XL", 
+  "Uganda Infrastructure XL"
+]
+const days = [
+  21, 
+  46, 
+  89, 
+  10, 
+  24
+]
+const dates = [
+  '11/12/19',
+  '10/11/20',
+  '09/07/18',
+  '06/01/19',
+  '14/03/21',
+  '11/12/19',
+  '10/11/20',
+  '09/07/18',
+  '06/01/19',
+  '14/03/21'
+]
 
 class Bond extends Component {
   constructor(props) {
@@ -85,7 +110,7 @@ class Bond extends Component {
       title: 'Info',
       content: (
         <div>
-          <p>Some info ab this ffuckin bond </p>
+          <p>Information about the bond</p>
         </div>
       ),
       okText: 'Ok',
@@ -96,25 +121,48 @@ class Bond extends Component {
   render() {
     return (
       <div>
-        <BondCard>
+        {/*<BondCard>
           <Name width={3/10} px={1} align="center">
-            Kenya Infrastructure
+            { names[this.props.bond - 1] }
           </Name>
           <Percent width={1/5} px={1} align="center">
-            50 % of portfolio
+            {percentages[this.props.bond - 1]} % of portfolio
           </Percent>
           <MatureTime width={1/10} px={1} align="center">
-            7 Years
+            Date: { dates[this.props.bond - 1] }
           </MatureTime>
           <Number width={1/10} px={1} align="center">
             5237
           </Number>
-          <BuyButton width={1/5} px={1} align="center" onClick={this.showModal}>
-            Buy
-          </BuyButton>
-          <InfoButton width={1/10} px={1} align="center" onClick={this.info}>
-            <Icon type="info" />
-          </InfoButton>
+          
+        </BondCard>*/}
+
+        <BondCard wrap >
+          <Box width={1} px={1} py={1} align="center">
+            <Progress percent={ percentages[this.props.bond - 1] } format={percent => `${percent}d`} />
+          </Box>
+          <BondInfo>
+            <Box width={3/10} px={1} align="center">
+              <h2>{ names[this.props.bond - 1] }</h2>
+            </Box>
+            <Box width={2/10} px={1} align="center">
+              { percentages[this.props.bond - 1] } % of portfolio
+            </Box>
+            <Box width={2/10} px={1} align="center">
+              <div>
+                { dates[this.props.bond - 1] }
+              </div>
+             </Box>
+            <Box width={1/10} px={1} align="center">
+              price
+            </Box>
+            <Button width={1/10} px={1} align="center" onClick={this.showModal} type="primary">
+              Buy
+            </Button>
+            <Button width={1/10} px={1} align="center" onClick={this.info} type="dashed">
+              <Icon type="info" />
+            </Button>
+          </BondInfo>
         </BondCard>
 
         <Modal
@@ -136,5 +184,78 @@ class Bond extends Component {
 }
 
 export default Bond
+
+
+
+
+
+
+
+
+
+
+
+/*
+import React from 'react'
+import styled from 'styled-components'
+import { Progress, Tooltip } from 'antd'
+import { sample } from 'lodash'
+import { Flex, Box } from 'grid-styled'
+import moment from 'moment'
+
+const BondCard = styled(Flex)`
+  box-shadow: 0 0 0 0px rgba(0,0,0,.1), 0 2px 3px rgba(0,0,0,.2);
+  padding: 5px 5px 5px 0px;
+  background: white;
+  border-left: 4px solid ${({ color }) => color};
+  border-radius: 4px;
+  width: 100%;
+  margin-bottom: 10px;
+`
+const BondName = styled.h6`
+  color: black;
+  font-size: 14px;
+`
+const percentages = [38, 22, 18, 6, 16]
+const days = [21, 46, 89, 10, 24]
+const dates = [
+  '11/12/19',
+  '10/11/20',
+  '09/07/18',
+  '06/01/19',
+  '14/03/21',
+]
+
+const Bond = ({ bond, bondNumber }) => {
+  console.log(bondNumber)
+  return (
+    <BondCard wrap color={sample(['#FF4C4C', '#33F4AA', '#F9DC5C'])}>
+      <Box width={1} px={1} py={1} align="center">
+        <Tooltip title="Number of days until you are paid">
+          <Progress percent={days[bondNumber]} format={percent => `${percent}d`} />
+        </Tooltip>
+      </Box>
+      <Box width={1/4} px={1} align="center">
+        <BondName>{bond.name}</BondName>
+      </Box>
+      <Box width={1/4} px={1} align="center">
+        {percentages[bondNumber]} % of portfolio
+      </Box>
+      <Box width={1/4} px={1} align="center">
+        <Tooltip title="When the bond will mature">
+          <div>
+            {dates[bondNumber]}
+          </div>
+        </Tooltip>
+      </Box>
+      <Box width={1/4} px={1} align="center">
+        ${bond.marketDirtyPrice}
+      </Box>
+    </BondCard>
+  )
+}
+
+export default Bond
+*/
 
 
--- src/components/buy/sellBondCard.js
+++ src/components/buy/sellBondCard.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
-import { Icon, Modal, Button, notification  } from 'antd'
+import { Icon, Modal, Button, notification, Progress  } from 'antd'
 import { Flex, Box } from 'grid-styled'
 
 
 const BondCard = styled.div `
   box-shadow: 0 0 0 0px rgba(0,0,0,.1), 0 2px 3px rgba(0,0,0,.2);
-  border: thin double #EDEDED;
-  padding: 5px;
+  padding: 5px 5px 5px 0px;
+  background: white;
+  border-left: 4px solid ${({ color }) => color};
+  border-radius: 4px;
   width: 100%;
-  height: 80px;
+  ${'' /* height: 80px; */}
   margin-bottom: 10px;
+`
+const BondInfo = styled.div `
   display: flex;
-  align-items: center;
-  justify-content: space-between;
 `
 
 const SellButton = styled.button `
@@ -46,7 +48,38 @@ const InfoButton = styled.button `
   height: 100%;
   background-color: #BBB;
 `
-
+const percentages = [12, 41, 38, 22, 18, 6, 16, 12, 22, 12]
+const names = [
+  "Uganda Infrastructure XL",
+  "Zambia Government XL", 
+  "Uganda Infrastructure XL",
+  "Zambia Government XL", 
+  "Uganda Infrastructure XL",
+  "Zambia Government XL", 
+  "Uganda Infrastructure XL",
+  "Zambia Government XL", 
+  "Uganda Infrastructure XL",
+  "Zambia Government XL"
+]
+const days = [
+  21,  
+  10, 
+  24,
+  46, 
+  89
+]
+const dates = [
+  '06/01/19',
+  '14/03/21',
+  '11/12/19',
+  '10/11/20',
+  '09/07/18',
+  '06/01/19',
+  '10/11/20',
+  '09/07/18',
+  '11/12/19',
+  '14/03/21'
+]
 
 class Bond extends Component {
   constructor(props) {
@@ -96,6 +129,7 @@ class Bond extends Component {
   render() {
     return (
       <div>
+      { /* <div>
         <BondCard>
           <Name width={3/10} px={1} align="center">
             Kenya Infrastructure
@@ -115,6 +149,34 @@ class Bond extends Component {
           <InfoButton width={1/10} px={1} align="center" onClick={this.info}>
             <Icon type="info" />
           </InfoButton>
+        </BondCard>
+      */ }
+        <BondCard wrap >
+          <Box width={1} px={1} py={1} align="center">
+            <Progress percent={ percentages[this.props.bond - 1] } format={percent => `${percent}d`} />
+          </Box>
+          <BondInfo>
+            <Box width={3/10} px={1} align="center">
+              <h2>{ names[this.props.bond - 1] }</h2>
+            </Box>
+            <Box width={2/10} px={1} align="center">
+              { percentages[this.props.bond - 1] } % of portfolio
+            </Box>
+            <Box width={2/10} px={1} align="center">
+              <div>
+                { dates[this.props.bond - 1] }
+              </div>
+             </Box>
+            <Box width={1/10} px={1} align="center">
+              price
+            </Box>
+            <Button width={1/10} px={1} align="center" onClick={this.showModal} type="primary">
+              Buy
+            </Button>
+            <Button width={1/10} px={1} align="center" onClick={this.info} type="dashed">
+              <Icon type="info" />
+            </Button>
+          </BondInfo>
         </BondCard>
 
         <Modal
